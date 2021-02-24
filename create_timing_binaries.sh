a19dfa79b9fa1127f469b4dfc233f236107acafa
vkostala <vatistas.kostalabros@bsc.es>
2021-02-24 12:16:52 +0200

    Changes to makefile and build binaries scripts to be applied as cherry pick commits to other branches

--- create_timing_binaries.sh
+++ create_timing_binaries.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-rm kat
+rm -rf timing_binaries_$1/
+
+mkdir timing_binaries_$1
+mkdir timing_binaries_$1/timing_arm/
+mkdir timing_binaries_$1/timing_x86/
+mkdir timing_binaries_$1/timing_arm/vec_$1/
+mkdir timing_binaries_$1/timing_arm/no_vec_$1/
+mkdir timing_binaries_$1/timing_x86/vec_$1/
+mkdir timing_binaries_$1/timing_x86/no_vec_$1/
 
 for opt_level in 0 1 2 3
 do
-	make kat ARCH=arm OPT=$opt_level VEC=y
-	mv kat timing_binaries/timing_arm/vec/kat_O$opt_level
+	make kat ARCH=arm OPT=$opt_level VEC=y KAT=$1
+	mv kat timing_binaries_$1/timing_arm/vec_$1/kat_O$opt_level
 
-	make kat ARCH=arm OPT=$opt_level VEC=n
-        mv kat timing_binaries/timing_arm/no_vec/kat_O$opt_level
+	make kat ARCH=arm OPT=$opt_level VEC=n KAT=$1
+        mv kat timing_binaries_$1/timing_arm/no_vec_$1/kat_O$opt_level
 
-	make kat OPT=$opt_level VEC=y
-        mv kat timing_binaries/timing_x86/vec/kat_O$opt_level
+	make kat OPT=$opt_level VEC=y KAT=$1
+        mv kat timing_binaries_$1/timing_x86/vec_$1/kat_O$opt_level
 
-        make kat OPT=$opt_level VEC=n
-        mv kat timing_binaries/timing_x86/no_vec/kat_O$opt_level
+        make kat OPT=$opt_level VEC=n KAT=$1
+        mv kat timing_binaries_$1/timing_x86/no_vec_$1/kat_O$opt_level
 
 
 done
