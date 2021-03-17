1e966800b8ab9564eb3bba633c53a5942e032d4c
vkostala <vatistas.kostalabros@bsc.es>
2021-03-18 00:08:31 +0200

    add 15 synd kernels

--- synd_15/synd_kernel15_13.c
+++ synd_15/synd_kernel15_13.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-gf gf_add_kernel10_1(gf in0, gf in1)
+
+gf gf_add_kernel15_13(gf in0, gf in1)
 {
 	return in0 ^ in1;
 }
 
-gf gf_mul_kernel10_1(gf in0, gf in1)
+gf gf_mul_kernel15_13(gf in0, gf in1)
 {
 	int i;
 
@@ -42,7 +43,7 @@ gf gf_mul_kernel10_1(gf in0, gf in1)
 
 /* input: field element in */
 /* return: (in^2)^2 */
-static inline gf gf_sq2_kernel10_1(gf in)
+static inline gf gf_sq2_kernel15_13(gf in)
 {
 	int i;
 
@@ -78,7 +79,7 @@ static inline gf gf_sq2_kernel10_1(gf in)
 
 /* input: field element in, m */
 /* return: (in^2)*m */
-static inline gf gf_sqmul_kernel10_1(gf in, gf m)
+static inline gf gf_sqmul_kernel15_13(gf in, gf m)
 {
 	int i;
 
@@ -119,7 +120,7 @@ static inline gf gf_sqmul_kernel10_1(gf in, gf m)
 
 /* input: field element in, m */
 /* return: ((in^2)^2)*m */
-static inline gf gf_sq2mul_kernel10_1(gf in, gf m)
+static inline gf gf_sq2mul_kernel15_13(gf in, gf m)
 {
 	int i;
 
@@ -163,31 +164,31 @@ static inline gf gf_sq2mul_kernel10_1(gf in, gf m)
 
 
 
-gf gf_frac_kernel10_1(gf den, gf num)
+gf gf_frac_kernel15_13(gf den, gf num)
 {
 	gf tmp_11;
 	gf tmp_1111;
 	gf out;
 
-	tmp_11 = gf_sqmul_kernel10_1(den, den); // ^11
-	tmp_1111 = gf_sq2mul_kernel10_1(tmp_11, tmp_11); // ^1111
-	out = gf_sq2_kernel10_1(tmp_1111);
-	out = gf_sq2mul_kernel10_1(out, tmp_1111); // ^11111111
-	out = gf_sq2_kernel10_1(out);
-	out = gf_sq2mul_kernel10_1(out, tmp_1111); // ^111111111111
+	tmp_11 = gf_sqmul_kernel15_13(den, den); // ^11
+	tmp_1111 = gf_sq2mul_kernel15_13(tmp_11, tmp_11); // ^1111
+	out = gf_sq2_kernel15_13(tmp_1111);
+	out = gf_sq2mul_kernel15_13(out, tmp_1111); // ^11111111
+	out = gf_sq2_kernel15_13(out);
+	out = gf_sq2mul_kernel15_13(out, tmp_1111); // ^111111111111
 
-	return gf_sqmul_kernel10_1(out, num); // ^1111111111110 = ^-1
+	return gf_sqmul_kernel15_13(out, num); // ^1111111111110 = ^-1
 }
 
 
-gf gf_inv_kernel10_1(gf den)
+gf gf_inv_kernel15_13(gf den)
 {
-	return gf_frac_kernel10_1(den, ((gf) 1));
+	return gf_frac_kernel15_13(den, ((gf) 1));
 }
 
 
 
-gf eval_inner10_1(gf *f, gf a)
+gf eval_inner15_13(gf *f, gf a)
 {
         int i;
         gf r;
@@ -198,19 +199,19 @@ gf eval_inner10_1(gf *f, gf a)
         {
 //		#pragma HLS PIPELINE II=3
 //		#pragma HLS unroll factor=2
-                r = gf_mul_kernel10_1(r, a) ^ f[i];
+                r = gf_mul_kernel15_13(r, a) ^ f[i];
         }
 
         return r;
 }
 
-void synd_kernel10_1(gf *out_out, gf *f_in, gf *L_in, unsigned char *r_in)
+void synd_kernel15_13(gf *out_out, gf *f_in, gf *L_in, unsigned char *r_in)
 {
 
-	#pragma HLS INTERFACE m_axi     port=out_out  offset=slave bundle=gmem0
-	#pragma HLS INTERFACE m_axi     port=f_in     offset=slave bundle=gmem1
-	#pragma HLS INTERFACE m_axi     port=L_in     offset=slave bundle=gmem2
-	#pragma HLS INTERFACE m_axi     port=r_in     offset=slave bundle=gmem3
+	#pragma HLS INTERFACE m_axi     port=out_out  offset=slave bundle=gmem4
+	#pragma HLS INTERFACE m_axi     port=f_in     offset=slave bundle=gmem5
+	#pragma HLS INTERFACE m_axi     port=L_in     offset=slave bundle=gmem6
+	#pragma HLS INTERFACE m_axi     port=r_in     offset=slave bundle=gmem7
 	#pragma HLS INTERFACE s_axilite port=out_out            bundle=control
 	#pragma HLS INTERFACE s_axilite port=f_in               bundle=control
 	#pragma HLS INTERFACE s_axilite port=L_in               bundle=control
@@ -223,14 +224,15 @@ void synd_kernel10_1(gf *out_out, gf *f_in, gf *L_in, unsigned char *r_in)
 	gf e, e_inv, c;
 	gf local_out[2*SYS_T];
 	gf local_f[SYS_T+1];
-	gf local_L[SYS_N/10];
-	unsigned char local_r[MAT_COLS/10];
+	gf local_L[SYS_N];
+	gf tmp_mul_1, tmp_mul_2;
+	unsigned char local_r[MAT_COLS];
 
 	gf e_mat[SYS_N];
 
-	#pragma HLS ARRAY_PARTITION variable=local_out cyclic factor=4 //64
-	#pragma HLS ARRAY_PARTITION variable=local_L cyclic factor=4 //8
-	#pragma HLS ARRAY_PARTITION variable=e_mat cyclic factor=4 //8
+	#pragma HLS ARRAY_PARTITION variable=local_out cyclic factor=4
+	#pragma HLS ARRAY_PARTITION variable=local_L cyclic factor=4
+	#pragma HLS ARRAY_PARTITION variable=e_mat cyclic factor=4
 
 	//READ into local vars
 
@@ -239,52 +241,50 @@ void synd_kernel10_1(gf *out_out, gf *f_in, gf *L_in, unsigned char *r_in)
 		local_f[i] = *(f_in+i);
 	}
 
-	LOOP_LOAD_FROM_BRAM_L:for (uint i=0;i<SYS_N/10;i++){
+	LOOP_LOAD_FROM_BRAM_L:for (uint i=12*SYS_N/15;i<13*SYS_N/15;i++){
 	#pragma HLS PIPELINE II=1
 	#pragma HLS unroll factor=4
 		local_L[i] = *(L_in+i);
 	}
 
-
-	LOOP_LOAD_FROM_BRAM_R:for (uint i=0;i<MAT_COLS/10;i++){
+	LOOP_LOAD_FROM_BRAM_R:for (uint i=12*MAT_COLS/15;i<13*MAT_COLS/15;i++){
 	#pragma HLS PIPELINE II=1
 //	#pragma HLS unroll factor=2
 		local_r[i] = *(r_in+i);
 	}
 
-
 	//READ into local vars END
 	LOOP_EVAL:
-	for(uint i=0; i <SYS_N/10; i++){//11
+	for(uint i=12*SYS_N/15; i <13*SYS_N/15; i++){
 //	#pragma HLS PIPELINE
 //	#pragma HLS unroll factor=2
-		e_mat[i] = eval_inner10_1(local_f, local_L[i]);
+		e_mat[i] = eval_inner15_13(local_f, local_L[i]);
 	}
 
 
 
 	LOOP_MAIN_OUTER:
-	for (uint i = 0; i < SYS_N/10; i++) //11
+	for (uint i = 12*SYS_N/15; i < 13*SYS_N/15; i++) //12
 	{
 //	#pragma HLS pipeline
 
 		c = (local_r[i>>3] >> (i%8)) & 1;
-		e_inv = gf_inv_kernel10_1(gf_mul_kernel10_1(e_mat[i],e_mat[i]));
+		e_inv = gf_inv_kernel15_13(gf_mul_kernel15_13(e_mat[i],e_mat[i]));
 
 		LOOP_MAIN_INNER:
-		for (uint j = 0; j < 2*SYS_T; j++) //8
+		for (uint j = 0; j < 2*SYS_T; j++)//15
 		{
 //		#pragma HLS DEPENDENCE inter variable=local_out false
 		#pragma HLS PIPELINE
 //		#pragma HLS unroll factor=32
 
-			if(i==0){
-				local_out[j] = gf_mul_kernel10_1(e_inv, c);
+			if(i==12*SYS_N/15){
+				local_out[j] = gf_mul_kernel15_13(e_inv, c);
 			}else{
-				local_out[j] ^= gf_mul_kernel10_1(e_inv, c);
+				local_out[j] ^= gf_mul_kernel15_13(e_inv, c);
 
 			}
-			e_inv = gf_mul_kernel10_1(e_inv, local_L[i]);
+			e_inv = gf_mul_kernel15_13(e_inv, local_L[i]);
 
 		}
 	}
