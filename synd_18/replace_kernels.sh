dd6886f2f35ef0a58af2da7a5886f2800f9d6037
vkostala <vatistas.kostalabros@bsc.es>
2021-03-24 10:25:57 +0200

    add dummy directory and replace kernels script

---
+++ synd_18/replace_kernels.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Give as arg $1 the multiplicity you want to procude ans as $2 the multiplicity you currently have
+for (( kernel_num=1; kernel_num<=$2; kernel_num++ ))
+do
+    sed  -e "s+/$2+/$1+g" -e  "s/$2_$kernel_num/$1_$kernel_num/g" synd_kernel$2_$kernel_num.c > synd_kernel$1_$kernel_num.c
+    echo "test_kernel_$1_$kernel_num"
+done
+
+start=$(($2 + 1))
+prev_mult=$(($2 - 1))
+for (( kernel_num=$start; kernel_num<=$1; kernel_num++ ))
+do
+    prev_idx=$(($kernel_num - 1)) 
+    sed  -e "s+/$2+/$1+g" -e  "s/$2_$2/$1_$kernel_num/g" -e  "s+$2\*+$kernel_num\*+g" -e "s+$prev_mult\*+$prev_idx\*+g"  synd_kernel$2_$2.c > synd_kernel$1_$kernel_num.c
+    echo "test_kernel_$1_$kernel_num"
+done
