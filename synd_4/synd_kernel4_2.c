1f7beb183eed4a689a485252a8ef71f566d28fce
vkostala <vatistas.kostalabros@bsc.es>
2021-03-24 10:41:23 +0200

    prepare synd kernels for run

--- synd_4/synd_kernel4_2.c
+++ synd_4/synd_kernel4_2.c
@@ -4,34 +4,33 @@
 #include <string.h>
 //#include "ap_cint.h"
 
-gf gf_add_kernel1_1(gf in0, gf in1)
+gf gf_add_kernel4_2(gf in0, gf in1)
 {
 	return in0 ^ in1;
 }
 
-gf gf_mul_kernel1_1(gf in0, gf in1)
+gf gf_mul_kernel4_2(gf in0, gf in1)
 {
 	int i;
 
 	uint32_t tmp;
+	uint32_t tmp_bit, tmp_mul;
 	uint32_t t0;
 	uint32_t t1;
 	uint32_t t;
 
 	t0 = in0;
 	t1 = in1;
-#pragma HLS dependence variable=tmp_mul_mat RAW true
+
 
 	tmp = t0 * (t1 & 1);
 
 	for (uint i = 1; i < GFBITS; i++){
-//#pragma HLS dependence variable=tmp_mul_mat RAW true
+//	#pragma HLS DEPENDENCE inter variable=tmp_mul RAW false
 //	#pragma HLS pipeline
 //	#pragma HLS unroll
 		tmp ^= (t0 * (t1 & (1 << i)));
 	}
-
-
 
 
 	t = tmp & 0x7FC000;
@@ -45,7 +44,7 @@ gf gf_mul_kernel1_1(gf in0, gf in1)
 	return tmp & ((1 << GFBITS)-1);
 }
 
-static inline gf gf_sq_kernel1_1(gf in)
+static inline gf gf_sq_kernel4_2(gf in)
 {
 	const uint32_t B[] = {0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF};
 
@@ -68,7 +67,7 @@ static inline gf gf_sq_kernel1_1(gf in)
 	return x & ((1 << GFBITS)-1);
 }
 
-gf gf_inv_kernel1_1(gf in)
+gf gf_inv_kernel4_2(gf in)
 {
 	gf tmp_11;
 	gf tmp_1111;
@@ -76,32 +75,32 @@ gf gf_inv_kernel1_1(gf in)
 	gf out = in;
 
 
-	out = gf_sq_kernel1_1(out);
-	tmp_11 = gf_mul_kernel1_1(out, in); // 11
+	out = gf_sq_kernel4_2(out);
+	tmp_11 = gf_mul_kernel4_2(out, in); // 11
 
-	out = gf_sq_kernel1_1(tmp_11);
-	out = gf_sq_kernel1_1(out);
-	tmp_1111 = gf_mul_kernel1_1(out, tmp_11); // 1111
+	out = gf_sq_kernel4_2(tmp_11);
+	out = gf_sq_kernel4_2(out);
+	tmp_1111 = gf_mul_kernel4_2(out, tmp_11); // 1111
 
-	out = gf_sq_kernel1_1(tmp_1111);
+	out = gf_sq_kernel4_2(tmp_1111);
 
-	out = gf_sq_kernel1_1(out);
-	out = gf_sq_kernel1_1(out);
-	out = gf_sq_kernel1_1(out);
+	out = gf_sq_kernel4_2(out);
+	out = gf_sq_kernel4_2(out);
+	out = gf_sq_kernel4_2(out);
 
-	out = gf_mul_kernel1_1(out, tmp_1111); // 11111111
+	out = gf_mul_kernel4_2(out, tmp_1111); // 11111111
 
-	out = gf_sq_kernel1_1(out);
-	out = gf_sq_kernel1_1(out);
-	out = gf_mul_kernel1_1(out, tmp_11); // 1111111111
+	out = gf_sq_kernel4_2(out);
+	out = gf_sq_kernel4_2(out);
+	out = gf_mul_kernel4_2(out, tmp_11); // 1111111111
 
-	out = gf_sq_kernel1_1(out);
-	out = gf_mul_kernel1_1(out, in); // 11111111111
+	out = gf_sq_kernel4_2(out);
+	out = gf_mul_kernel4_2(out, in); // 11111111111
 
-	return gf_sq_kernel1_1(out); // 111111111110
+	return gf_sq_kernel4_2(out); // 111111111110
 }
 
-gf eval_inner1_1(gf *f, gf a)
+gf eval_inner4_2(gf *f, gf a)
 {
         int i;
         gf r;
@@ -110,21 +109,22 @@ gf eval_inner1_1(gf *f, gf a)
 
         for (i = SYS_T-1; i >= 0; i--)
         {
-//		#pragma HLS PIPELINE II=3
+//		#pragma HLS PIPELINE II=1
 //		#pragma HLS unroll factor=2
-                r = gf_mul_kernel1_1(r, a) ^ f[i];
+                r = gf_mul_kernel4_2(r, a) ^ f[i];
+//                r = gf_add(r, f[i]);
         }
 
         return r;
 }
 
-void synd_kernel1_1(gf *out_out, gf *f_in, gf *L_in, unsigned char *r_in)
+void synd_kernel4_2(gf *out_out, gf *f_in, gf *L_in, unsigned char *r_in)
 {
 
-	#pragma HLS INTERFACE m_axi     port=out_out  offset=slave bundle=gmem0
+	#pragma HLS INTERFACE m_axi     port=out_out  offset=slave bundle=gmem1
 	#pragma HLS INTERFACE m_axi     port=f_in     offset=slave bundle=gmem1
-	#pragma HLS INTERFACE m_axi     port=L_in     offset=slave bundle=gmem2
-	#pragma HLS INTERFACE m_axi     port=r_in     offset=slave bundle=gmem3
+	#pragma HLS INTERFACE m_axi     port=L_in     offset=slave bundle=gmem1
+	#pragma HLS INTERFACE m_axi     port=r_in     offset=slave bundle=gmem1
 	#pragma HLS INTERFACE s_axilite port=out_out            bundle=control
 	#pragma HLS INTERFACE s_axilite port=f_in               bundle=control
 	#pragma HLS INTERFACE s_axilite port=L_in               bundle=control
@@ -134,16 +134,16 @@ void synd_kernel1_1(gf *out_out, gf *f_in, gf *L_in, unsigned char *r_in)
 	#pragma HLS inline recursive
 
 	int i, j;
-	gf e, c;
-	gf e_inv;
+	gf e, e_inv, c;
 	gf local_out[2*SYS_T];
 	gf local_f[SYS_T+1];
 	gf local_L[SYS_N];
+	gf tmp_mul_1, tmp_mul_2;
 	unsigned char local_r[MAT_COLS];
 
 	gf e_mat[SYS_N];
 
-	#pragma HLS ARRAY_PARTITION variable=local_out cyclic factor=4 //32
+	#pragma HLS ARRAY_PARTITION variable=local_out cyclic factor=4 //64
 	#pragma HLS ARRAY_PARTITION variable=local_L cyclic factor=4 //8
 	#pragma HLS ARRAY_PARTITION variable=e_mat cyclic factor=4 //8
 
@@ -154,52 +154,50 @@ void synd_kernel1_1(gf *out_out, gf *f_in, gf *L_in, unsigned char *r_in)
 		local_f[i] = *(f_in+i);
 	}
 
-	LOOP_LOAD_FROM_BRAM_L:for (uint i=0;i<SYS_N;i++){
+	LOOP_LOAD_FROM_BRAM_L:for (uint i=1*SYS_N/4;i<2*SYS_N/4;i++){
 	#pragma HLS PIPELINE II=1
 	#pragma HLS unroll factor=4
 		local_L[i] = *(L_in+i);
 	}
 
-
-	LOOP_LOAD_FROM_BRAM_R:for (uint i=0;i<MAT_COLS;i++){
+	LOOP_LOAD_FROM_BRAM_R:for (uint i=1*MAT_COLS/4;i<2*MAT_COLS/4;i++){
 	#pragma HLS PIPELINE II=1
 	#pragma HLS unroll factor=2
 		local_r[i] = *(r_in+i);
 	}
 
-
 	//READ into local vars END
 	LOOP_EVAL:
-	for(uint i=0; i <SYS_N; i++){
+	for(uint i=1*SYS_N/4; i <2*SYS_N/4; i++){
 //	#pragma HLS PIPELINE
-//	#pragma HLS unroll factor=2 //taking long
-		e_mat[i] = eval_inner1_1(local_f, local_L[i]);
+//	#pragma HLS unroll factor=2
+		e_mat[i] = eval_inner4_2(local_f, local_L[i]);
 	}
 
 
+
 	LOOP_MAIN_OUTER:
-	for (uint i = 0; i < SYS_N; i++)
+	for (uint i = 1*SYS_N/4; i < 2*SYS_N/4; i++) //12
 	{
-//	#pragma HLS pipeline
+	// #pragma HLS pipeline
 
 		c = (local_r[i>>3] >> (i%8)) & 1;
-		e_inv = gf_inv_kernel1_1(gf_mul_kernel1_1(e_mat[i],e_mat[i]));
+		e_inv = gf_inv_kernel4_2(gf_mul_kernel4_2(e_mat[i],e_mat[i]));
 
 		LOOP_MAIN_INNER:
-		for (uint j = 0; j < 2*SYS_T; j++)
+		for (uint j = 0; j < 2*SYS_T; j++)//8
 		{
-//		#pragma HLS DEPENDENCE inter variable=e_inv RAW true
+//		#pragma HLS DEPENDENCE inter variable=local_out false
 		#pragma HLS PIPELINE
-//		#pragma HLS unroll factor=2
+//		#pragma HLS unroll factor=32
 
-
-			if(i==0){
-				local_out[j] = gf_mul_kernel1_1(e_inv, c);
+			if(i==1*SYS_N/4){
+				local_out[j] = gf_mul_kernel4_2(e_inv, c);
 			}else{
-				local_out[j] ^= gf_mul_kernel1_1(e_inv, c);
-			}
+				local_out[j] ^= gf_mul_kernel4_2(e_inv, c);
 
-			e_inv = gf_mul_kernel1_1(e_inv, local_L[i]);
+			}
+			e_inv = gf_mul_kernel4_2(e_inv, local_L[i]);
 
 		}
 	}
